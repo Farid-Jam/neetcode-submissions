7a5324d594ebe9c4039ac5a84c88fe7912d2ea56
Farid <farid.jamshid77@gmail.com>
2026-08-06 12:55:33 -0400

    Add: anagram-groups - submission-10

---
+++ Data Structures & Algorithms/anagram-groups/submission-10.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        freqMap = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            freqMap[tuple(count)].append(s)
+        return [group for group in freqMap.values()]
